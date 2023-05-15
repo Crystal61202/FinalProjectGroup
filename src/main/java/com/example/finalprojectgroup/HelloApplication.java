74cd22135e36c4ce66f983805d6ad802ef36bafb
Crystal61202 <130778810+Crystal61202@users.noreply.github.com>
2023-05-15 22:58:42 +0700

    Update HelloApplication.java

--- src/main/java/com/example/finalprojectgroup/HelloApplication.java
+++ src/main/java/com/example/finalprojectgroup/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        // Testing Khang
+        // Testing Khang JAVA 
     }
 
     public static void main(String[] args) {

f2c396fab3ad648efa0b681258ab24f73e965ac5
khang <s3929513@rmit.edu.vn>
2023-05-15 23:08:39 +0700

    Update HelloApplication.java

--- src/main/java/com/example/finalprojectgroup/HelloApplication.java
+++ src/main/java/com/example/finalprojectgroup/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        // Testing Khang JAVA Hello Worldd Khang Minh Truong Tran Minh Truong 12345
+        // Testing Khang JAVA Hello Worldd Khang Minh Truong Tran Minh Truong 1234567
     }
 
     public static void main(String[] args) {

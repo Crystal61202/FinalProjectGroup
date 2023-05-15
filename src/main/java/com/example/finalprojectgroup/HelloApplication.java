e3c2aac9abc02cce2d0370c965d7d54fbf3cfa7e
crystal <s3891643@rmit.edu.vn>
2023-05-15 23:05:40 +0700

    update

--- src/main/java/com/example/finalprojectgroup/HelloApplication.java
+++ src/main/java/com/example/finalprojectgroup/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-        // Testing Khang JAVA Hello Worldd Khang 
+        // Testing Khang JAVA Hello Worldd Khang Minh Truong
     }
 
     public static void main(String[] args) {

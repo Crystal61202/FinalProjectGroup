8b7951df08cf81d2e2373ec9f880ed07a003cb4a
tringo135 <131856162+tringo135@users.noreply.github.com>
2023-05-17 20:43:45 +0700

    Main.java

--- src/main/java/com/example/finalprojectgroup/Main.java
+++ src/main/java/com/example/finalprojectgroup/Main.java
@@ -20,7 +20,20 @@ public class Main {
         for (Item item : list) {
             System.out.println(item);
         }
+        
+        
+        Customer customer = new Customer("C001", "Minh Dinh", "18 Irwin Street", "0421473243", "minhdinh", "123456");
+        Rental rental1 = new Rental("I001-2001", 1);
+        Rental rental2 = new Rental("I002-1988", 1);
 
+        customer.addRecord(rental1);
+        customer.addRecord(rental2);
+
+        ArrayList<Rental> records = customer.getRecords();
+
+        for (Rental record : records) {
+            System.out.println(record);
+        }
 
     }
 }

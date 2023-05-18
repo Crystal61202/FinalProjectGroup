77263116919b1f70e8e9f9792a32702b1fb4c4ef
tringo135 <131856162+tringo135@users.noreply.github.com>
2023-05-18 09:16:27 +0700

    VIPAccount.java

---
+++ src/main/java/com/example/finalprojectgroup/VIPAccount.java
@@ -0,0 +1,21 @@
+package com.example.finalprojectgroup;
+
+import java.io.Serializable;
+
+public class VIPAccount extends Customer implements Serializable {
+
+    private int rewardPoints;
+
+    public VIPAccount(String ID, String name, String address, String phone, String username, String password) {
+        super(ID, name, address, phone, username, password);
+        this.rewardPoints = 0;
+    }
+
+    @Override
+    public void addRewardPoints(int points) {
+        this.rewardPoints += points;
+        if (this.rewardPoints >= 100) {
+            System.out.println("You have enough reward points to rent 1 item for free.");
+        }
+    }
+}

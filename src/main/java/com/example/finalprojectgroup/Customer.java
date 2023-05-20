f7e6f4815d5bda8d39c4129c667e8244f77af899
tringo135 <131856162+tringo135@users.noreply.github.com>
2023-05-20 10:14:43 +0700

    Customer.java

--- src/main/java/com/example/finalprojectgroup/Customer.java
+++ src/main/java/com/example/finalprojectgroup/Customer.java
@@ -1,16 +1,26 @@
 package com.example.finalprojectgroup;
-import java.util.ArrayList;
 
-public class Customer {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public abstract class Customer implements java.io.Serializable {
     private String ID;
     private String name;
     private String address;
     private String phone;
-    private ArrayList<Rental> rentals;
+    private static String accountType;
+    private ArrayList<Item> rentals;
     private String username;
     private String password;
+    public int Number_pay=0;
+    public int Max_rent=0;
+    public int promote_condition=0;
 
-    public Customer(String ID, String name, String address, String phone, String username, String password) {
+
+    public Customer(){
+
+    }
+    public Customer(String ID, String name, String address, String phone, String accountType,String username, String password) {
         if (!isValidID(ID)) {
             throw new IllegalArgumentException("Invalid ID format.");
         }
@@ -18,35 +28,35 @@ public class Customer {
         this.name = name;
         this.address = address;
         this.phone = phone;
+        setAccountType(accountType);
         this.rentals = new ArrayList<>();
         this.username = username;
         this.password = password;
-
     }
+
 
     public String getID() {
         return ID;
     }
-
+    public void SetID(String a){
+        ID=a;
+    }
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
+    public void SetName(String name) {
         this.name = name;
     }
-
     public String getAddress() {
         return address;
     }
-
-    public void setAddress(String address) {
+    public void SetAddress(String address) {
         this.address = address;
     }
-
     public String getPhone() {
         return phone;
     }
+    public void SetPhone(String phone) {this.phone = phone;}
 
     public void setPhone(String phone) {
         this.phone = phone;
@@ -60,81 +70,115 @@ public class Customer {
     public void setUsername(String username){
         this.username = username;
     }
-
-    public ArrayList<Rental> getRentals() {
-        return rentals;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public void addRental(Rental rental) {
-        if (this instanceof GuestAccount && rentals.size() >= 2 && rental.getDays() == 2) {
-            System.out.println("Error: Guest account can only rent 1-day items.");
-        } else {
-            rentals.add(rental);
-            if (this instanceof VIPAccount) {
-                ((VIPAccount) this).addRewardPoints(10);
-            }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAccountType(){
+        return accountType;
+    }
+    public void setAccountType(String accountType) {
+        String[] availableAccountType = {"GuestAccount","RegularAccount","ViPAccount"};
+        if (Arrays.asList(availableAccountType).contains(accountType)) {
+            this.accountType = accountType;
+        } else{
+            throw new IllegalArgumentException("rent type is not valid, 3 available rent type are DVD, VideoGame, OldMovieRecord");
         }
     }
 
-    public String getUsername() {
-        return username;
+
+    public ArrayList<Item> getRentals() {return rentals;}
+    public void SetRentals(ArrayList<Item> rental){this.rentals=rental;}
+    public String getUsername() {return username;}
+    public void SetUsername(String a){username=a;}
+    public String getPassword() {return password;}
+    public void SetPassword(String a){password=a;}
+    public static boolean isValidID(String ID) {return ID.matches("^C\\d{3}$");}
+
+
+    public static class GuestAccount extends Customer implements java.io.Serializable {
+        public GuestAccount(){
+
+        }
+
+        public GuestAccount(String ID, String name, String address, String phone, String accountType, String username, String password) {
+            super(ID, name, address, phone, accountType, username, password);
+            Max_rent=2;
+            promote_condition=3;
+        }
+
+
+
+
+        public int addRental(Item rental) {
+            if (getRentals().size() == Max_rent) {
+            return 0;
+            //System.out.println("Error: Guest account can only rent 1-day items.");
+            }
+            if (rental.getLoanType()=="2-day"){
+            return -1;
+            }
+            getRentals().add(rental);
+            return 1;
+        }
+        //__2 promote ___ 0 false ____ 1 true
+        public int remoteRental(Item rental){
+            for (Item r : getRentals())
+                if (rental.getTitle() == r.getTitle()){
+                    getRentals().remove(r);
+                    Number_pay+=1;
+                    //promote account
+                    if(Number_pay>promote_condition){
+                        return 2;
+                    }
+                    return 1;
+                }
+            return 0;
+        }
     }
+    public static class RegularAccount extends GuestAccount implements  java.io.Serializable {
+        public RegularAccount(){
 
-    public String getPassword() {
-        return password;
+        }
+
+        public RegularAccount(String ID, String name, String address, String phone, String accountType, String username, String password){
+            super(ID, name, address, phone, accountType, username, password);
+            promote_condition=5;
+        }
+
+
+
+        @Override
+        public int addRental(Item rental) {
+            getRentals().add(rental);
+            return 1;
+        }
     }
+    public static class VIPAccount extends RegularAccount implements java.io.Serializable {
+        private int rewardPoints=0;
+        private int rent_free=0;
+        public VIPAccount(){
 
-    public static boolean isValidID(String ID) {
-        return ID.matches("^C\\d{3}$");
-    }
+        }
 
-    public void setAccountType(String selectedCustomer) {
-    }
-
-
-    public String getID(String ID) {
-        return ID;
-    }
-}
-
-class Rental{
-    private String itemID;
-    private int days;
+        public VIPAccount(String ID, String name, String address, String phone, String accountType, String username, String password) {
+            super(ID, name, address, phone, accountType, username, password);
+        }
 
 
-    public Rental(String itemID, int days) {
 
-        this.itemID = itemID;
-        this.days = days;
-    }
-
-    public String getItemID() {
-        return itemID;
-    }
-
-    public int getDays() {
-        return days;
-    }
-
-
-}
-class CustomerList extends Customer{
-
-    private ArrayList<Customer> customers;
-
-    public CustomerList(String ID, String name, String address, String phone, String username, String password, String itemID, int days) {
-        super(ID, name, address, phone, username, password);
-        customers = new ArrayList<>();
-    }
-
-    public void addCustomer(Customer customer) {
-        customers.add(customer);
-    }
-
-    public ArrayList<Customer> getCustomers() {
-        return customers;
-    }
-    public String getID(String ID) {
-        return ID;
+        public int addRental(Item rental) {
+            getRentals().add(rental);
+            this.rewardPoints += 10;
+            //reward---------------------------
+            if (this.rewardPoints >= 100) {
+                rewardPoints=0;
+                rent_free+=1;
+                //System.out.println("You have enough reward points to rent 1 item for free.");
+            }
+            return 1;
+        }
     }
 }
